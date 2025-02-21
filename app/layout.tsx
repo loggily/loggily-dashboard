562c1ccb6f82934c18a5c33adbd045ab907900b3
ksapiratheepan <ksapiratheepan@users.noreply.github.com>
2025-02-21 16:33:51 +0530

    Add dashboard header

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,18 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="fixed inset-x-0 top-0 z-10 border-b border-emerald-500/25 dark:border-white/10">
+          <div className="bg-white dark:bg-gray-950">
+            <div className="flex h-14 items-center justify-between gap-8 px-4 sm:px-6">
+              <div className="flex gap-4">
+                <span className="text-xl font-bold text-emerald-500 shrink-0">Loggily Dashboard</span>
+              </div>
+              <div className="flex items-center gap-6 max-md:hidden">
+                Dark/Light?
+              </div>
+            </div>
+          </div>
+        </div>
         {children}
       </body>
     </html>
