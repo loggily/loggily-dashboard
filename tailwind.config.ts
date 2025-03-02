4da488359dcc06d99e75ae99edb88aa1e4d09788
ksapiratheepan <ksapiratheepan@users.noreply.github.com>
2025-03-02 16:31:33 +0530

    Add tooltip component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,7 @@ export default {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@heroui/theme/dist/components/(button|ripple|spinner).js"
+    "./node_modules/@heroui/theme/dist/components/(button|popover|ripple|spinner).js"
   ],
   theme: {
     extend: {
