55077cbd23cb0d0d5646e6e2bd1ae22062f8320f
ksapiratheepan <ksapiratheepan@users.noreply.github.com>
2025-03-18 11:27:33 +0530

    Add table component from heroui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,7 @@ export default {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@heroui/theme/dist/components/(autocomplete|button|input|popover|ripple|spinner|form|listbox|divider|scroll-shadow).js"
+    "./node_modules/@heroui/theme/dist/components/(autocomplete|button|input|table|popover|ripple|spinner|form|listbox|divider|scroll-shadow|checkbox|spacer).js"
   ],
   theme: {
     extend: {
